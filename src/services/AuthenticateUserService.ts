65ead8389db6316b7a269c2d98f2a7f421b0c0cd
Rafa Martins <rafamartins.dev@gmail.com>
2021-10-20 10:25:15 -0300

    services

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import prismaClient from "../prisma";
+
+import { sign } from "jsonwebtoken";
 
 /*
 . Receber code(string)
@@ -6,7 +9,7 @@ import axios from "axios";
 . Recuperar infos do user no github
 . Verificar se o usuário existe no DB
   - SIM = Gera um token
-  - NÃO = Cria no DB, gera um token
+  - NÃO = Criar no DB, gera um token
 . Retornar o token com as infos do user
 */
 
@@ -46,7 +49,41 @@ class AuthenticateUserService {
       }
     );
 
-    return response.data;
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: +id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: +id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
